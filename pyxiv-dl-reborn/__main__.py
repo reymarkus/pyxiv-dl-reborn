bde24250a8fbcdae548c47921089f0555c8ae08a
ReyMarkus <elreydevera@tutanota.com>
2020-01-10 23:41:47 +0800

    Added initial dirty work

---
+++ pyxiv-dl-reborn/__main__.py
@@ -0,0 +1,66 @@
+"""pyxiv-dl-reborn main script
+
+This is the main script that executes the main pyxiv-dl-reborn argument parser.
+"""
+
+import argparse
+from imagerip import PixivImageRipper
+
+# constants
+
+"""Script version"""
+PYXIVDL_VERSION = "0.1.0"
+
+"""Main function for """
+def main():
+    # load argparse here
+    argParser = argparse.ArgumentParser(
+        description="pyxiv-dl: Downloads full-sized arts from Pixiv",
+        usage="pyxiv-dl.py [options] urls..."
+    )
+    
+    # add verbose argument
+    argParser.add_argument(
+        "-v",
+        "--verbose",
+        help="show verbose output",
+        action="store_true"
+    )
+    
+    # add NSFW confirmation bypass
+    argParser.add_argument(
+        "-n",
+        "--nsfw",
+        help="Always allow NSFW image download. If not set, you are asked to confirm the download",
+        action="store_true"
+    )
+
+    # show script version
+    argParser.add_argument(
+        "-V",
+        "--version",
+        help="Show the application's version and exit",
+        action="version",
+        version="%(prog)s v{}".format(PYXIVDL_VERSION)
+    )
+
+    # main argument: pixiv art IDs
+    argParser.add_argument(
+        "ids",
+        help="your Pixiv medium IDs to get original images",
+        type=str,
+        action="append",
+        nargs="+"
+    )
+
+    # set parsed args variable
+    parsedArgs = argParser.parse_args()
+
+    ###########
+    # runner
+    ##########
+    PixivImageRipper(parsedArgs[0], parsedArgs.verbose, parsedArgs.nsfw)
+
+# main call
+if __name__ == "__main__":
+    main()
---
+++ pyxiv-dl-reborn/imagerip.py
@@ -0,0 +1,40 @@
+import sys, requests, re as regex
+from urllib.parse import urlparse
+
+class PixivImageRipper():
+    """Main image ripper class"""
+
+    # class constants
+    """Base Pixiv URL, which is also used as a referrer"""
+    PIXIV_URL = "https://www.pixiv.net"
+
+    """Request headers for use with requests library"""
+    REQUEST_HEADERS = {
+        "referer": PIXIV_URL,
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0"
+    }
+
+    """Initialize downloader"""
+    def __init__(self, pixivIds=[], verbose=False, bypassNsfw=False):
+        # check for pixiv IDs first
+        self._checkPixivIds(pixivIds)
+
+    """Checks the input if they are a valid Pixiv ID"""
+    def _checkPixivIds(self, pixivIds=[]):
+        # pixiv IDs are 6-digits long and currently at 700,000 arts and
+        # counting. Let's assume first that it will grow to 1M+ arts
+        # throughout the year
+
+        #compile the regex for checking numbers
+        pxIdRegex = regex.compile("^[0-9]{7}$", regex.I)
+
+        for ids in pixivIds:
+            pxIdCheck = pxIdRegex.match(ids)
+            try:
+                if pxIdCheck is not None:
+                    pass
+                else:
+                    print("ERROR: one or more inputs is not a valid Pixiv ID. Aborting.")
+                    sys.exit(1)
+            except IndexError:
+                print("ERROR: something went wrong with checking the Pixiv IDs. Aborting")
